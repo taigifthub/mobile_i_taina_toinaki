1c893a428ab556ac8fa8302807db9a5882d9b313
taigifthub <tainamarcondes07@gmail.com>
2024-02-29 08:35:13 -0300

    Aula 02 sobre variáveis

---
+++ aula02/variaveis.js
@@ -0,0 +1,24 @@
+var numero = 2;
+let texto = "Hello,";
+const oQueEisso = "?";
+
+//O ponto e vírgula é opcional. Boas práticas o usam ♥
+
+console.log("Tipo var: ", numero)
+
+numero = 13; 
+
+console.log(texto + " o número  agora é " + numero)
+
+texto = 69
+
+console.log(texto + numero)
+//concatenar com + dois valores numericos vai acabar somando
+
+// console.log(oQueEisso)
+
+//Constantes não podem ter novos valores assinalados a ela
+
+// oQueEisso = "uma constante???????"
+
+// console.log(oQueEisso)
