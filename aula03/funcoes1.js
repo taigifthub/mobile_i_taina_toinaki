c3a0823dcfba8739f544d9062e704301c3a38835
taigifthub <tainamarcondes07@gmail.com>
2024-03-14 08:25:00 -0300

    Aula 03 sobre funções e métodos.

---
+++ aula03/funcoes1.js
@@ -0,0 +1,15 @@
+function Soma () {
+    let n1 = 2;
+    let n2 = 6;
+
+    console.log(n1+n2);
+}
+
+Soma()
+
+function SomaComParametros (n1, n2) {
+    console.log(n1 + n2)
+}
+
+SomaComParametros(8, 10)
+SomaComParametros(50, 10)
---
+++ aula03/metodos.js
@@ -0,0 +1,13 @@
+let txt = "Hello amigos";
+
+console.log(txt.length)
+
+console.log(txt.slice(6, 12))
+
+console.log(txt.replace("Hello", "Olá"))
+
+let newTxt = txt.replace("Hello", "Olá")
+
+console.warn(newTxt.toUpperCase())
+
+console.warn(newTxt.toLowerCase())
